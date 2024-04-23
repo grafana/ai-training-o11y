05ff51a46b42903a032a50a89c989a47601e1b32
Annanay Agarwal <annanay25@users.noreply.github.com>
2024-04-23 15:19:48 -0700

    Metadata schema (#14)
    * checkpoint
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>
    
    * Add nested JSON support for process metadata
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>
    
    ---------
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>

---
+++ ai-training-api/model/metadata.go
@@ -0,0 +1,19 @@
+package model
+
+import "github.com/google/uuid"
+
+// MetadataKV is the database model used to track metadata information.
+// This is used to flatten JSON metadata into a key-value pair and index
+// it for search.
+type MetadataKV struct {
+	// Tenant ID is used to identify the tenant to which the metadata belongs.
+	TenantID string `json:"tenant_id"`
+	// Key is the metadata key.
+	Key string `json:"key"`
+	// Value is the metadata value.
+	Value string `json:"value"`
+
+	// Process ID is the UUID of the process to which the metadata belongs.
+	// Its the foreign key to the Process table.
+	ProcessID uuid.UUID `json:"process_id"`
+}
--- ai-training-api/model/process.go
+++ ai-training-api/model/process.go
@@ -1,20 +1,25 @@
 package model
 
 import (
+	"time"
+
 	"github.com/google/uuid"
-	"gorm.io/datatypes"
 )
 
 type Process struct {
+	// UUID generated for the process.
+	ID uuid.UUID `json:"process_uuid" gorm:"primarykey;type:char(36)" validate:"isdefault"`
 	// Tenant ID is used to identify the tenant to which the process belongs.
 	TenantID string `json:"tenant_id"`
-	// UUID generated for the process.
-	ID uuid.UUID `json:"process_uuid"`
 	// The process status.
 	Status string `json:"status"`
+	// Start time.
+	StartTime time.Time `json:"start_time"`
+	// End time.
+	EndTime time.Time `json:"end_time"`
 
 	// Process Metadata.
 	// This field is used to store additional metadata about the process.
 	// TODO: cap at 1024?
-	Metadata datatypes.JSON `json:"metadata"`
+	Metadata []MetadataKV `json:"-"`
 }
