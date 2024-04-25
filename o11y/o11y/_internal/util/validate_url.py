e48f1703a10399e370bf7c43ad21039486b8270f
Aaron Sanders <aaron.sanders@grafana.com>
2024-04-25 12:09:01 -0500

    Add jupyter env (#16)
    * Add vendor to the ai-training-api makefile
    
    * Add call to api makefile from main, add docker command to makefile
    
    * Update readme to specify best way to bring dev env up
    
    * Update deps in readme
    
    * Make makefile more comprehensive (has weird error)
    
    * Remove "server" target from plugin build
    
    * Fix makefile
    
    * Add docker-down
    
    * Fix error in last commit
    
    * Commit jupyter notebook to docker-compose
    
    * Update .gitignore for jupyter
    
    * Fix url validation
    
    * Add jupyter server and notebook
    
    Squash this
    
    * Add jupyter window launch to makefile
    
    * Bugfixes
    
    * Update outputs
    
    * Fix port assignments in docker compose
    
    * Remove unused regex lib
    
    * Swap a name
    
    * Switch port for ai-training-api

---
+++ o11y/o11y/_internal/util/validate_url.py
@@ -0,0 +1,23 @@
+import re
+
+url_regex = re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+    r'localhost|' #localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+def validate_url(url):
+    """
+    Validate a URL.
+
+    Args:
+        url (str): The URL to validate.
+
+    Returns:
+        bool: True if the URL is valid, False otherwise.
+    """
+
+    return bool(url_regex.match(url))
+
