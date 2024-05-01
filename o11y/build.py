ecc8b6fae38c9e38364521bd9c422b9576018fbe
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-01 14:51:30 -0500

    Switch build from poetry to rye with hatchling backend

--- o11y/build.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="o11y",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=["requests", "pydantic", "python-logging-loki"],
-    extras_require={"dev": ["pytest"]}
-)
