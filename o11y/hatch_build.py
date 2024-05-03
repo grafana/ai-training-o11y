33dad245598e6775dd198d76f00f8cca1d4159b2
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-03 16:53:41 -0500

    Finally got a build hook that works correctly for wheels

--- o11y/hatch_build.py
+++ o11y/hatch_build.py
@@ -1,19 +1,21 @@
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+import pathlib
 import shutil
 import subprocess
 import os
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict):
+        print("Buildhookran")
         build_data['pure_python'] = False
-        self.build_go()
+        build_data['artifacts'].extend(["src/o11y"])
+        go_bin = self.build_go()
+        build_data['force_include'][go_bin] = "/go-plugin/go-plugin"
     
     def build_go(self):
         go_bin = shutil.which('go')
         if not go_bin:
             self.app.abort('Go is not installed, go is required to build from source')
-        
-        # self.print_file_tree(os.getcwd())
 
         subprocess.check_call(
             [
@@ -21,6 +23,9 @@ class CustomBuildHook(BuildHookInterface):
                 'build',
                 '-o',
                 './dist/go-plugin',
+                '-v',
             ],
         cwd='./go-plugin/'
         )
+        
+        return pathlib.Path("go-plugin", "dist", "go-plugin").as_posix()
