52142ae52a3cdad944b46681b5d4462b8373a267
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-06 09:24:06 -0500

    Set wheel tag correctly in hatch build

--- o11y/hatch_build.py
+++ o11y/hatch_build.py
@@ -2,6 +2,8 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 import pathlib
 import shutil
 import subprocess
+import re
+import sysconfig
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict):
@@ -9,8 +11,39 @@ class CustomBuildHook(BuildHookInterface):
         build_data['artifacts'].extend(["src/o11y"])
         go_bin = self.build_go()
         build_data['force_include'][go_bin] = "/o11y/go-plugin"
-        # TODO: Set platform version in the wheel
-        # Make it possible to do platform builds based on an env variable
+        build_data['tag'] = f"py3-none-{self._get_platform_tag()}"
+        # TODO: Make it possible to do platform builds based on an env variable
+
+    # Substantially identical to the same function from wandb's exporter
+    # It turns out this is a pretty tricky problem
+    # Used because MIT license allows it
+    def _get_platform_tag(self) -> str:
+        """Returns the platform tag for the current platform."""
+        # Replace dots, spaces and dashes with underscores following
+        # https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/#platform-tag
+        platform_tag = re.sub("[-. ]", "_", sysconfig.get_platform())
+
+        # On macOS versions >=11, pip expects the minor version to be 0:
+        #   https://github.com/pypa/packaging/issues/435
+        #
+        # You can see the list of tags that pip would support on your machine
+        # using `pip debug --verbose`. On my macOS, get_platform() returns
+        # 14.1, but `pip debug --verbose` reports only these py3 tags with 14:
+        #
+        # * py3-none-macosx_14_0_arm64
+        # * py3-none-macosx_14_0_universal2
+        #
+        # We do this remapping here because otherwise, it's possible for `pip wheel`
+        # to successfully produce a wheel that you then cannot `pip install` on the
+        # same machine.
+        macos_match = re.fullmatch(r"macosx_(\d+_\d+)_(\w+)", platform_tag)
+        if macos_match:
+            major, _ = macos_match.group(1).split("_")
+            if int(major) >= 11:
+                arch = macos_match.group(2)
+                platform_tag = f"macosx_{major}_0_{arch}"
+
+        return platform_tag
 
     def build_go(self):
         go_bin = shutil.which('go')
