48015ffbd8b7ff0e1e5bce5fa5668edceefd9096
Annanay Agarwal <annanay25@users.noreply.github.com>
2024-05-06 13:28:28 -0700

    Add admin template and endpoint for single process (#30)
    * Add admin template and endpoint for single process
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>
    
    * Fix panic
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>
    
    ---------
    
    Signed-off-by: Annanay Agarwal <annanay.agarwal@grafana.com>

--- ai-training-api/app/admin.go
+++ ai-training-api/app/admin.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/grafana/ai-training-o11y/ai-training-api/model"
 )
@@ -14,12 +15,16 @@ import (
 //go:embed templates/processes.html
 var processesPageContent string
 
+//go:embed templates/process.html
+var processPageContent string
+
 //go:embed templates/style.css
 var stylesheetContent string
 
 type Admin struct {
 	app                   *App
 	processesPageTemplate *template.Template
+	processPageTemplate   *template.Template
 }
 
 func NewAdmin(app *App) *Admin {
@@ -38,14 +43,20 @@ func NewAdmin(app *App) *Admin {
 	tProcesses.Funcs(funcs)
 	processesPageTemplate := template.Must(tProcesses.Parse(processesPageContent))
 
+	tProcess := template.New("process")
+	tProcess.Funcs(funcs)
+	processPageTemplate := template.Must(tProcess.Parse(processPageContent))
+
 	return &Admin{
 		app:                   app,
 		processesPageTemplate: processesPageTemplate,
+		processPageTemplate:   processPageTemplate,
 	}
 }
 
 func (admin *Admin) Register(router *mux.Router) {
 	router.HandleFunc("/processes", admin.processes).Methods("GET")
+	router.HandleFunc("/process/{id}", admin.process).Methods("GET")
 }
 
 type listResponse[T any] struct {
@@ -85,6 +96,43 @@ func (a *Admin) processes(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	err = a.processesPageTemplate.Execute(w, resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (a *Admin) process(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	processId := namedParam(req, "id")
+	query := req.URL.Query()
+	tenantID := query.Get("tenant")
+
+	process := model.Process{}
+	err := a.app.db(ctx).
+		Unscoped().
+		Where(&model.Process{ID: uuid.MustParse(processId), TenantID: tenantID}).
+		Find(&process).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// find all associated metadata
+	err = a.app.db(ctx).
+		Where(model.MetadataKV{TenantID: tenantID, ProcessID: uuid.MustParse(processId)}).
+		Find(&process.Metadata).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type processResponse struct {
+		Process model.Process
+	}
+
+	data := processResponse{process}
+	err = a.processPageTemplate.Execute(w, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
