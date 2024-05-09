603b0ec5670ae019280a7ab6fb5d8967f2591fd9
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-09 10:12:31 -0500

    Set up datasource proxy to call metadata service from frontend (#37)
    * Fix config load for grafana spinup
    
    * Add proxy
    
    * Add reverse proxy
    
    * Remove dummy endpoint
    
    * Have datasource call proxy correctly
    
    * Tidy up scene
    
    * Tidy up readme
    
    * Fix a url

--- grafana-aitraining-app/pkg/plugin/resources.go
+++ grafana-aitraining-app/pkg/plugin/resources.go
@@ -3,6 +3,10 @@ package plugin
 import (
 	"encoding/json"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
 
 // handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
@@ -37,25 +41,24 @@ func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (a *App) handleProjects(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
+func metadataHandler(target string) func(http.ResponseWriter, *http.Request) {
+	remote, err := url.Parse(target)
+	if err != nil {
+		log.DefaultLogger.Error(err.Error())		
 	}
-	projectsMap := []map[string]string{{"name": "project1"}, {"name": "project2"}, {"name": "project3"}, {"name": "project4"}}
-	// Same as above, but four individual json objects
-	w.Header().Add("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(projectsMap); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	p := httputil.NewSingleHostReverseProxy(remote)
 
-	w.WriteHeader(http.StatusOK)
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Remove the string "/metadata" from the request URL
+		r.URL.Path = r.URL.Path[len("/metadata"):]
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		p.ServeHTTP(w, r)
+	}
 }
 
 // registerRoutes takes a *http.ServeMux and registers some HTTP handlers.
 func (a *App) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/ping", a.handlePing)
 	mux.HandleFunc("/echo", a.handleEcho)
-	mux.HandleFunc("/projects", a.handleProjects)
+	mux.HandleFunc("/metadata/", metadataHandler("http://ai-training-api:8000")) // Use config for this
 }
