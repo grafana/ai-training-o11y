bd69f1e7f5d753d7f8cb09f95683d8a6d85f123b
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-08 23:16:28 -0500

    Have datasource call proxy correctly

--- grafana-aitraining-app/src/datasource/Datasource.tsx
+++ grafana-aitraining-app/src/datasource/Datasource.tsx
@@ -4,32 +4,62 @@ import { RuntimeDataSource } from "@grafana/scenes";
 import { DataQuery } from "@grafana/schema";
 import { Observable, lastValueFrom } from "rxjs";
 
-export const getProjects = (apiPrefix: string): Promise<DataQueryResponseData> => {
-  return lastValueFrom(getBackendSrv().fetch({
-    url: `${apiPrefix}/resources/projects`,
-    method: 'GET',
-  })).then((response) => {
+type JSONPrimitive = string | number | boolean | null;
+type JSONObject = { [key: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = JSONPrimitive | JSONObject | JSONArray;
+
+// Actually make a request from the plugin backend
+function doRequest(fetchOptions: any): Promise<DataQueryResponseData> {
+  return lastValueFrom(getBackendSrv().fetch(fetchOptions)).then((response) => {
     if (!response.ok) {
       throw response.data;
     }
     return response?.data;
-  })
-};
+  });
+}
 
+// Getter for endpoints without payload
+export function makeGetRequester(apiPrefix: string): () => Promise<DataQueryResponseData> {
+  return () => {
+    const fetchOptions: any = {
+      url: `${apiPrefix}`,
+      method: 'GET',
+    };
+
+    return doRequest(fetchOptions);
+  };
+}
+
+// Getter for endpoints with payload
+export function makeGetRequesterWithPayload<T extends JSONObject>(apiPrefix: string): (payload: T) => Promise<DataQueryResponseData> {
+  return (payload: T) => {
+    const fetchOptions: any = {
+      url: `${apiPrefix}`,
+      method: 'GET',
+      data: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return doRequest(fetchOptions);
+  };
+}
 
 export class TrainingApiDatasource extends RuntimeDataSource {
-  private apiPrefix: string;
+  private getProcesses: () => Promise<DataQueryResponseData>;
 
   constructor(datasourceId: string, datasourceUid: string, pluginId: string) {
     super(datasourceId, datasourceUid);
-    this.apiPrefix = `/api/plugins/${pluginId}`;
+    this.getProcesses = makeGetRequester(`/api/plugins/${pluginId}/resources/metadata/api/v1/processes`);
   }
 
   query(request: DataQueryRequest<DataQuery>): Promise<DataQueryResponse> | Observable<DataQueryResponse> {
-    return getProjects(this.apiPrefix).then((projects) => {
+    return this.getProcesses().then((projects) => {
       return {
         data: [
-          arrayToDataFrame(projects),
+          arrayToDataFrame(projects.data),
         ],
         state: LoadingState.Done,
       };
