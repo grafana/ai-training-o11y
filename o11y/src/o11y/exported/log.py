bc4e943485dd1344dca000c5e654b81c8272f7e8
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-10 09:21:31 -0500

    Add x_axis for custom x_axis and section for custom sections in exporter

--- o11y/src/o11y/exported/log.py
+++ o11y/src/o11y/exported/log.py
@@ -1,13 +1,13 @@
 from .. import client
 from .. import logger
 
-def log(log):
+def log(log, *, x_axis=None, section=None):
     """
     Sends a log to the Loki server
     :param log: The log message
     :return: None
     """
-    # Check that log is a dict with all keys strings not containing .
+    # Check that log is a dict with all keys strings
     if not isinstance(log, dict):
         logger.error("Log must be a dict")
         return False
@@ -15,13 +15,42 @@ def log(log):
         if not isinstance(key, str):
             logger.error("Keys in log must be strings")
             return False
-        if chr(31) in key:
-            logger.error("Keys in log must not contain the unit separator character")
-            return False
+
     # Check that all values are numbers
     for key in log.keys():
         if not isinstance(log[key], (int, float)):
             logger.error("Values in log must be numbers")
             return False
+        
+    # check that section is of type string
+    if section is not None and not isinstance(section, str):
+        logger.error("Section must be a string")
+        return False
+    
+    if x_axis is None:
+        client.send_model_metrics(log, section=section)
+        return
+    
+    # Check if x_axis exists
+    if not isinstance(x_axis, dict) or len(x_axis) != 1:
+        logger.error("x_axis must be a dict with one key")
+        return False
+    
+    # Check that x_axis' single key is a string, and its value is a number
+    x_key = list(x_axis.keys())[0]
+    x_value = x_axis[x_key]
+    if not isinstance(x_key, str):
+        logger.error("x_axis key must be a string")
+        return False
+    if not isinstance(x_value, (int, float)):
+        logger.error("x_axis value must be a number")
+        return False
+    
+    # Check that this key is not already in the log line
+    if x_key in log.keys() and x_value != log[x_key]:
+        logger.error("x_axis key must not be in your metrics, or must have the same value")
+        return False
+    
 
-    client.send_model_metrics(log)
+
+    client.send_model_metrics(log, x_axis=x_axis, section=section)
