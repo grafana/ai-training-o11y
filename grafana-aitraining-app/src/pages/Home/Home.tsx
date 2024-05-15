c846153869acbfd3214b2d0b4aa0ddf240ac058f
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-15 02:02:29 -0500

    Basic routing setup

--- grafana-aitraining-app/src/pages/Home/Home.tsx
+++ grafana-aitraining-app/src/pages/Home/Home.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import { getScene } from './Scene';
+import { LinkButton, useStyles2 } from '@grafana/ui';
+import { useParams } from 'react-router-dom';
+import { PluginPage } from '@grafana/runtime';
+import { testIds } from '../../components/testIds';
+import { prefixRoute } from 'utils/utils.routing';
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
+
 
 export const Home = () => {
-  const scene = getScene();
+  const params = useParams();
+  console.log('Path arguments:', params);
+  const s = useStyles2(getStyles);
 
-  return <scene.Component model={scene} />;
+
+  return (
+    <PluginPage>
+      <div data-testid={testIds.pageOne.container}>
+        This is page one.
+        <div className={s.marginTop}>
+          <LinkButton data-testid={testIds.pageOne.navigateToFour} href={prefixRoute('two')}>
+            Full-width page example
+          </LinkButton>
+        </div>
+      </div>
+    </PluginPage>
+  );
 };
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  marginTop: css`
+    margin-top: ${theme.spacing(2)};
+  `,
+});
