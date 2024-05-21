e1ae811bb29b50f7501c00808cbb77d339a9767f
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-20 21:55:49 -0500

    Make react table more legible

--- grafana-aitraining-app/src/pages/Home/components/Table.tsx
+++ grafana-aitraining-app/src/pages/Home/components/Table.tsx
@@ -1,9 +1,6 @@
 // Table.tsx
 import React from 'react';
-
-interface RowData {
-  [key: string]: string | undefined;
-}
+import { RowData } from 'utils/state';
 
 interface TableProps {
   data: RowData[];
@@ -11,26 +8,28 @@ interface TableProps {
 
 export const Table: React.FC<TableProps> = ({ data }) => {
   // Get the unique column names from the data
-  const columnNames = Array.from(
-    new Set(data.flatMap(Object.keys))
-  );
+  const columnNames = Array.from(new Set(data.flatMap(Object.keys)));
 
   return (
     <div>
       <h2>Table</h2>
-      <table>
+      <table style={styles.table}>
         <thead>
           <tr>
             {columnNames.map((column, index) => (
-              <th key={index}>{column}</th>
+              <th key={index} style={styles.th}>
+                {column}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
           {data.map((item, index) => (
-            <tr key={index}>
+            <tr key={index} style={styles.tr}>
               {columnNames.map((column, columnIndex) => (
-                <td key={columnIndex}>{item[column]}</td>
+                <td key={columnIndex} style={styles.td}>
+                  {item[column]}
+                </td>
               ))}
             </tr>
           ))}
@@ -38,6 +37,26 @@ export const Table: React.FC<TableProps> = ({ data }) => {
       </table>
     </div>
   );
+};
+
+const styles = {
+  table: {
+    borderCollapse: 'collapse' as 'collapse', // This is now a specific string value
+    width: '100%',
+  },
+  th: {
+    backgroundColor: 'gray',
+    border: '1px solid black',
+    padding: '10px',
+    textAlign: 'center' as 'center', // This is now a specific string value
+  },
+  tr: {
+    borderBottom: '1px solid #ddd',
+  },
+  td: {
+    border: '1px solid #ddd',
+    padding: '8px',
+  },
 };
 
 export default Table;
