3ed2478782603b985b701fdd1d8bc73e22070059
Aaron Sanders <aaron.sanders@grafana.com>
2024-05-22 23:41:47 -0500

    Switch process table over to backend fetch

--- grafana-aitraining-app/src/pages/Home/Home.tsx
+++ grafana-aitraining-app/src/pages/Home/Home.tsx
@@ -4,41 +4,10 @@ import { useParams } from 'react-router-dom';
 import { PluginPage } from '@grafana/runtime';
 import { prefixRoute } from 'utils/utils.routing';
 import { PageLayoutType } from '@grafana/data';
-import { RowData, useTrainingAppStore } from 'utils/state';
+import { useTrainingAppStore } from 'utils/state';
 import { Graphs } from './components/Graphs';
 import { TableTab } from './components/TableTab';
-
-const fetchTableData: () => RowData[] = () => {
-  return [
-    {
-      process_uuid: 'abc123',
-      status: 'running',
-      start_time: '2023-06-08T10:00:00',
-      additional_field1: 'Value 1',
-    },
-    {
-      process_uuid: 'def456',
-      status: 'finished',
-      start_time: '2023-06-08T11:30:00',
-      end_time: '2023-06-08T12:15:00',
-      additional_field2: 'Value 2',
-    },
-    {
-      process_uuid: 'ghi789',
-      status: 'crashed',
-      start_time: '2023-06-08T13:45:00',
-      end_time: '2023-06-08T14:00:00',
-      additional_field3: 'Value 3',
-    },
-    {
-      process_uuid: 'jkl012',
-      status: 'timed out',
-      start_time: '2023-06-08T15:20:00',
-      end_time: '2023-06-08T16:00:00',
-      additional_field4: 'Value 4',
-    },
-  ];
-};
+import { useGetProcesses } from 'utils/utils.plugin';
 
 export const Home = () => {
   // state management
@@ -63,14 +32,23 @@ export const Home = () => {
     setTab(tabFromUrl as "table" | "graphs");
   }, [tabFromUrl, setTab]);
 
-  // This will need to be made more elaborate for paging, filtering, grouping, etc.
+  const getProcesses = useGetProcesses();
+
+  // This will fetch the processes from the backend
+  // It will ultimately need to be more elaborate for paging, filtering, grouping, etc.
   useEffect(() => {
-    // Fetch table data and update the rows state
-    const tableData = fetchTableData();
-    setRenderedRows(tableData);
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  , []);
+    const fetchProcesses = async () => {
+      try {
+        const response = await getProcesses();
+        const data = response.data;
+        setRenderedRows(data);
+      } catch (error) {
+        console.error('Error fetching processes:', error);
+      }
+    };
+
+    fetchProcesses();
+  }, [getProcesses, setRenderedRows]);
 
   return (
     <PluginPage
