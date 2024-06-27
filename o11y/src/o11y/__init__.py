0149c45b4db734db37221e362b429229ec19c9c6
Aaron Sanders <aaron.sanders@grafana.com>
2024-06-27 17:00:22 -0500

    Fix naming on o11y go bits

--- o11y/src/o11y/__init__.py
+++ o11y/src/o11y/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 import subprocess
 
 resouce_package = __name__
-resource_path = 'go-plugin'
+resource_path = 'o11y-go'
 
 # run the binary from resource_path
 subprocess.run([pkg_resources.resource_filename(resouce_package, resource_path)])
