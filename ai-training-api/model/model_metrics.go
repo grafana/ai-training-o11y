812d834b7940d7f122a39067bf58e156bac8d446
Aaron Sanders <aaron.sanders@grafana.com>
2024-10-23 08:29:21 -0500

    Run gofmt

--- ai-training-api/model/model_metrics.go
+++ ai-training-api/model/model_metrics.go
@@ -12,12 +12,12 @@ import (
 // data for the same metric and step in one panel
 // Step is the step number, which goes on the x-axis, and MetricValue is the y-value.
 type ModelMetrics struct {
-    TenantID     string   `json:"stack_id" gorm:"not null;primaryKey"`
-    ProcessID   uuid.UUID `json:"process_id" gorm:"type:char(36);not null;primaryKey;foreignKey:ProcessID;references:ID"` // Foreign key
-    MetricName  string   `json:"metric_name" gorm:"size:32;not null;primaryKey"`
-    StepName    string   `json:"step_name" gorm:"size:32;not null;primaryKey"`
-    Step        uint32   `json:"step" gorm:"not null;primaryKey"`
-    MetricValue string   `json:"metric_value" gorm:"size:64;not null"`
+	TenantID    string    `json:"stack_id" gorm:"not null;primaryKey"`
+	ProcessID   uuid.UUID `json:"process_id" gorm:"type:char(36);not null;primaryKey;foreignKey:ProcessID;references:ID"` // Foreign key
+	MetricName  string    `json:"metric_name" gorm:"size:32;not null;primaryKey"`
+	StepName    string    `json:"step_name" gorm:"size:32;not null;primaryKey"`
+	Step        uint32    `json:"step" gorm:"not null;primaryKey"`
+	MetricValue string    `json:"metric_value" gorm:"size:64;not null"`
 
-    Process Process `gorm:"foreignKey:ProcessID;references:ID"` // Relationship definition
+	Process Process `gorm:"foreignKey:ProcessID;references:ID"` // Relationship definition
 }
