a39bac1a31c4fa3ccb7d5cc8b7e37dad701aeaa1
Aaron Sanders <aaron.sanders@grafana.com>
2024-10-25 14:12:54 -0500

    Make sure unit tests run properly on the golang binary

--- o11y/src/o11y/_internal/o11y_go.py
+++ o11y/src/o11y/_internal/o11y_go.py
@@ -1,11 +1,31 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import pkg_resources
 import subprocess
+import os
+import pkg_resources
+import pathlib
 
-resouce_package = __name__
-resource_path = 'o11y-go'
 
 def run():
-    # run the binary from resource_path
-    subprocess.run([pkg_resources.resource_filename(resouce_package, resource_path)])
+    if os.environ.get('HATCH_ENV_ACTIVE') or os.environ.get('HATCH'):
+        current_dir = pathlib.Path(__file__).parent
+        # Go up one level and then to the binary
+        binary_path = str(current_dir.parent.parent / "o11y-go" / "dist" / "o11y-go")
+    else:
+        binary_path = pkg_resources.resource_filename(__name__, 'o11y-go')
+    # Duplicate the real stdout/stderr file descriptors
+    stdout_copy = os.dup(1)  # 1 is stdout
+    stderr_copy = os.dup(2)  # 2 is stderr
+    
+    # Launch the binary with a pipe for stdin
+    process = subprocess.Popen(
+        [binary_path],
+        stdin=subprocess.PIPE,
+        pass_fds=(stdout_copy, stderr_copy)  # Pass our duplicated fds to the child
+    )
+    
+    # Send the descriptor numbers to the Go process
+    process.stdin.write(f"{stdout_copy} {stderr_copy}\n".encode())
+    process.stdin.flush()
+    
+    return process
